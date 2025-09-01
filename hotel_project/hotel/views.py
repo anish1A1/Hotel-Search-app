0575b345a5107247351dd1bb30ddf099cebf9d4f
anish1A1 <alloozpat87@gmail.com>
2025-09-01 19:31:24 +0545

    using ammenity as it has Many to Many Field. also used for filtering

--- hotel_project/hotel/views.py
+++ hotel_project/hotel/views.py
@@ -17,13 +17,23 @@ def get_hotel(request):
         if request.GET.get('amount'):
             amount = request.GET.get('amount')
             hotel_obj = hotel_obj.filter(hotel_price__lte = amount)
+            
+        if request.GET.get('ammenity'):
+            amenities = request.GET.get('ammenity')
+            amenities = str(amenities).split(',')
+            am = []
+            for amenitys in amenities:
+                am.append(amenitys)
+            hotel_obj = hotel_obj.filter(amentities__in = am)
         payload = []
+        
         for hotel in hotel_obj:
             payload.append({
                 'hotel_name' : hotel.hotel_name,
                 'hotel_price': hotel.hotel_price,
                 'hotel_description': hotel.hotel_description,
                 'banner_image': str(hotel.banner_image),
+                'ammenity': hotel.get_ammenity(),
             })
         return JsonResponse(payload, safe= False)
             
